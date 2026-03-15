de46c3eb8c1d4df7c10fdf644f2ca50d178dd55c
VoidXDev <developershiva18@gmail.com>
2026-03-15 20:10:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+from flask import Flask
 import threading
-from http.server import HTTPServer, BaseHTTPRequestHandler
 import os
 import asyncio
 import time
@@ -23,19 +23,15 @@ from telegram.ext import (
 )
 
 from pymongo import MongoClient
+flask_app = Flask(__name__)
 
-class Handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write(b"Bot running")
+@flask_app.route("/")
+def home():
+    return "Bot is running!"
 
-def run_server():
+def run_flask():
     port = int(os.environ.get("PORT", 10000))
-    server = HTTPServer(("0.0.0.0", port), Handler)
-    server.serve_forever()
-
-threading.Thread(target=run_server).start()
+    flask_app.run(host="0.0.0.0", port=port)
 # ==============================
 # CONFIG
 # ==============================
@@ -1340,6 +1336,8 @@ app.add_handler(CallbackQueryHandler(contact_admin, pattern="contact_admin"))
 
 if __name__ == "__main__":
 
-    print("Bot is running...")
+    threading.Thread(target=run_flask).start()
+
+    print("Bot running...")
 
     app.run_polling()
